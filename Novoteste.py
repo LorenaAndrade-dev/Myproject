782180db3e0e31745649e84784894267c1fa9abc
LorenaAndrade-dev <andradesantos.lorena1@gmail.com>
2024-07-12 11:15:22 -0300

    Novo teste de operadores matematicos

--- Novoteste.py
+++ Novoteste.py
@@ -1,1 +1,15 @@
-print("demonstração")
+#Demostração do uso de operadores
+X = int(input("Digite um número: "))
+Y = int(input("Digite um número: "))
+
+SOMA =  X + Y
+print("O resultao da soma é:", SOMA)
+
+SUBTRACAO = X - Y
+print("O resultado da subtração  é:", SUBTRACAO)
+
+MULTIPLICACAO = X * Y
+print("O resultado da multiplicação é:", MULTIPLICACAO)
+
+DIVISAO = X / Y
+print("O resultado da divisão é:", DIVISAO)
