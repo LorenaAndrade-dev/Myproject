36e2acd0ca957b551043e0aaa02349a3353f5011
LorenaAndrade-dev <andradesantos.lorena1@gmail.com>
2024-07-15 09:13:52 -0300

    atividade da aula

--- Operacoes.py
+++ Operacoes.py
@@ -6,7 +6,7 @@ x = 10; y = 20; z = 30
 VALOR = x * (y / 10 + z / 10) - 25
 print("O resultado da expressão 1 é:", VALOR)
 
-VALOR = ((y -x) + (z - x)) / 3
+VALOR = ((y - x) + (z - x)) / 3
 print("O resultado da espressão 2 é:", VALOR)
 
 VALOR = (x + (y * 2 + (z /10))) - 18
