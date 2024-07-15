09b5d13faf7dec9f1fadfd4daa42e72bcecf7652
LorenaAndrade-dev <andradesantos.lorena1@gmail.com>
2024-07-15 09:49:07 -0300

    Conversor de temperaturas

---
+++ Atividade5.py
@@ -0,0 +1,23 @@
+#Conversor de temperatura
+
+#Celsius
+temp = int(input("Digite a temperatura em Celsius:"))
+kelvin = temp + 273
+fah = 1,8 * temp + 32
+print(f"A conversão de Celsius para Kelvin é:{kelvin}")
+print(f"A conversão de Celsius para Fahrenheit é{fah}")
+
+#Kelvin
+temp = int(input("Digite a temperatura em Kelvin:"))
+celsius = temp - 273
+fah = (temp - 273) * 1,8 + 32
+print(f"O valor convertido de Kelvin para celsius é: {celsius}")
+print(f"O valor convertido de Kelvin para Fahrenheit é de: {fah}")
+
+#Fahrenheit
+temp = int(input("Digite a temperatura em Fahrenheit:"))
+celsius = (temp-32)/1,8
+kelvin = (temp-32)*5/9+273
+print(f"O valor convertido de Fahrenheit para celsius é de : {celsius}")
+print(f"O valor convertido de Fahrenheit para Kelvin é de {kelvin}")
+
