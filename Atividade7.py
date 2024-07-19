3dc093f777a18709231c6fe764fdd0cfdd964208
LorenaAndrade-dev <andradesantos.lorena1@gmail.com>
2024-07-19 08:50:30 -0300

    Continuação dos exercícios pedidos

--- Atividade7.py
+++ Atividade7.py
@@ -1,7 +1,7 @@
 # 1. Duas notas, se média <4 "reprovado", > 6 "aprovado direto", <4>6 "recuperação". Caso recuperação 
 # solicitar nota de recuperação, caso <5 " reprovado na recuperação", caso não "aprovado recuperação"
 
-#2 algoritmo para time e sua poição na tabela de classificação, exibir campeão para 1º lugar, libertadores ( entre 1 e 6)
+#2 algoritmo para time e sua posição na tabela de classificação, exibir campeão para 1º lugar, libertadores ( entre 1 e 6)
 #Sul america (entre 7 e 12) e rebaixado os demais
 
 #digitar 3 numeros distintos,sendo armazenados nas variavei x y z, informar se estão em ordem crescente ou decrescente, 
@@ -14,14 +14,51 @@ nota2 = float(input("Digite sua nota 2 aqui:"))
 media = nota1 + nota2 /2
 print(f"Sua média é de:{media}")
 
-
 if media < 4:
-    print( "Reprovado")
-elif media > 6:
-    print( "Aprovado direto")
-else:
+    print("Reprovado")
+elif media >= 4 and media < 6:
     print( "Recuperação")
-    
+    recuperacao = float(input("Digite a nota da recuperação:"))
+    if recuperacao < 5:
+        print ("Reprovado na recuperação")
+    else:
+        print(" Aprovado na recuperação")
+else:
+    print("Aprovado")
 
+    
+#2.
+time = input(" Digite seu time:")
+posicao = int(input(" Digite a posição do seu time:"))
+
+if posicao == 1:
+    print (" Campeão")
+elif posicao >=2 and posicao <6:
+    print("Libertadores")
+elif posicao >= 7 and posicao ==12:
+    print(" Sul - Americana")
+else:
+    print("Rebaixado")
+
+
+
+
+
+
+
+
+
+
+# 3.
+x = int(input(" Digite o primeiro número:"))
+y = int(input("Digite o segundo número:"))
+z = int(input("Digite o terceito número:"))
+
+if x > y and y > z:
+    print(" Números em ordem decrescente")
+elif x < y and y < z :
+    print(" Números em ondem cescente")
+else:
+    print ("Números misturados")
 
 
