48f30a49457d27f8a9a5a97d0d497b495f4c22f1
LorenaAndrade.dev <andradesantos.lorena1@gmail.com>
2024-08-05 11:18:58 -0300

    atividade

---
+++ Atividade10.py
@@ -0,0 +1,8 @@
+#Contador regressivo para ano novo, e exibir mensagem feliz ano novo.
+
+print("Digite a contagem:")
+numero = int(input())
+
+for i in range (0, numero):
+    print(i)
+print("Feliz ano novo!")
