1856de64236d39cc2f9cd0a4eeb69afdadd9bf89
LorenaAndrade.dev <andradesantos.lorena1@gmail.com>
2024-08-05 11:10:35 -0300

    atividades

---
+++ Atividade8.py
@@ -0,0 +1,6 @@
+#Construa um programa onde um usuário digitará um valor e o programa mostará na tela, a tabuada de multiplicação desse números. 
+#Se possivel,promova uma impressão que possibilite visualizar da melhor maneira possivel.
+
+
+
+
