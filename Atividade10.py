bd539b391e9a084d62d9ac7793576f5b2ea04933
LorenaAndrade-dev <andradesantos.lorena1@gmail.com>
2024-08-07 08:40:17 -0300

    resposta do exercício

--- Atividade10.py
+++ Atividade10.py
@@ -1,7 +1,8 @@
 #Contador regressivo para ano novo, e exibir mensagem feliz ano novo.
 
-numero = 0
-while numero <= 0:
-    print("Digite aqui a contagem:")
-    numero = int(input())
-print("Feliz ano novo")
+contador = 10
+
+while contador >= 0:
+    print("Contagem regressiva", contador)
+    contador = contador -1
+print("Feliz ano novo!")
