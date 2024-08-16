ab48b07803f6c2e16fc115f69bf9676f9688114b
LorenaAndrade-dev <andradesantos.lorena1@gmail.com>
2024-08-16 09:47:08 -0300

    edicao

--- cardapiohotel.py
+++ cardapiohotel.py
@@ -2,6 +2,8 @@ cafe = []
 almoco =[]
 jantar = []
 
+print("Criando seu cardápio peronalizado")
+
 print("Qual opção você deseja para o café da manhã?")
 for x in range (0,3):
     cafe.append(input(f"Escolha o {x+1}º opção:"))
@@ -19,7 +21,7 @@ print("As opções escolhidas foram: ", almoco)
 print("Qual opção você deseja de Jantar?")
 for x in range(0,4):
     jantar.append(input(f"Escolha a {x+1}º opção:"))
-    if jantar [x] == "Camarão" or almoco [x] == "Lagosta":
+    if jantar [x] == "Camarão" or jantar [x] == "Lagosta":
         print(" Esse alimento não é recomendado para pessoas com alergia a frutos do mar")
 print("As opções escolhidas foram: ", jantar)
 
